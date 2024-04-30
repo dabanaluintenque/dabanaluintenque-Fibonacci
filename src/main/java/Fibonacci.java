fd1039db5be2996b082b08e42b3bc586069737ba
Dabana Intenque <Dabanaluintenque55@gmail.com>
2024-04-30 20:30:36 +0000

    Coding Challenge: Fibonacci
    Done!

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,11 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+
+        if (n == 1 || n == 2) {
+           
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 }
